e8c381da1e29803c8d64bf0eaf848acb4c8d9190
Jia Voon Chong <jiavoonchong@gmail.com>
2023-11-05 02:24:29 +0800

    change css in home page - show product and buy btn to navigate to payment page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="d-flex align-center flex-column">
+    <v-card
+      class="justify-center"
+      width="400"
+      title="Product 1"
+      subtitle="Summary about product 1"
+      text="Description about product 1">
+      <v-btn class="mb-3" color="#5865f2"><RouterLink to="/payment">Buy</RouterLink></v-btn>
+    </v-card>
+    <!-- <TheWelcome /> -->
   </main>
 </template>
