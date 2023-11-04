b26bff0a277ac873b260a483ba42ed24b92b1fb6
Jia Voon Chong <jiavoonchong@gmail.com>
2023-11-05 02:19:05 +0800

    add payment page

---
+++ src/views/PaymentView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-80">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        label="First name"
+        :rules="firstNameRules"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="lastName"
+        label="Last name"
+        :rules="lastNameRules"
+      ></v-text-field>
+
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </div>
+</template>
+<script>
+export default {
+    data: () => ({
+      firstName: '',
+      firstNameRules: [
+        value => {
+          if (value?.length > 3) return true
+
+          return 'First name must be at least 3 characters.'
+        },
+      ],
+      lastName: '123',
+      lastNameRules: [
+        value => {
+          if (/[^0-9]/.test(value)) return true
+
+          return 'Last name can not contain digits.'
+        },
+      ],
+    }),
+  }
+</script>
+<style lang="scss">
+
+</style>
